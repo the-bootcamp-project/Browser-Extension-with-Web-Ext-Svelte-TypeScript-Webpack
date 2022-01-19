5bd1dd5f37cdd61ea587d915f0c1eb9287775e82
John Ollhorn <john@ollhorn.de>
2022-01-19 06:57:04 +0100

    Chore (Scripts): Add Basic Script for Extension

--- src/scripts/DevtoolsPage.ts
+++ src/scripts/DevtoolsPage.ts
@@ -1,13 +1,18 @@
-import browser from 'webextension-polyfill';
+import browser from 'webextension-polyfill'
 
 function handleShown() {
-    console.log('panel is being shown');
+	console.log('panel is being shown')
 }
 function handleHidden() {
-    console.log('panel is being hidden');
+	console.log('panel is being hidden')
 }
 
-browser.devtools.panels.create('My Panel', 'icons/star.png', 'devtools.html').then((newPanel) => {
-    newPanel.onShown.addListener(handleShown);
-    newPanel.onHidden.addListener(handleHidden);
-});
+browser.devtools.panels
+	.create('My Panel', 'icons/star.png', 'devtools.html')
+	.then(newPanel => {
+		newPanel.onShown.addListener(handleShown)
+		newPanel.onHidden.addListener(handleHidden)
+	})
+	.catch(err => {
+		console.log(err)
+	})
