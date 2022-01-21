f3ed5ba9b53cc09c547dd4a3cac325ff9e52c4a1
John Ollhorn <john@ollhorn.de>
2022-01-21 07:33:27 +0100

    Chore (Linting): Add Webextension in Linting Env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@
 const ESlintConfig = require('@bootcamp-project/svelte-config').ESLintSvelteTypeScriptConfig
 module.exports = {
     ...ESlintConfig,
+    env: {
+        ...ESlintConfig.env,
+        "webextensions": true
+    },
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
@@ -11,6 +15,6 @@ module.exports = {
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-		'pii/no-dob': 'off',
+        'pii/no-dob': 'off',
     }
 }
